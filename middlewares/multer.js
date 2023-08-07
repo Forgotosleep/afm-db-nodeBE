4abfc1ad08098f4c645d75a9683eb3649eff5daa
Brian <markbrian33@gmail.com>
2023-08-07 10:35:24 +0000

    Removed some revealing clogs in multer

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -6,26 +6,26 @@ const storage = multer.memoryStorage();
 const limits = { fileSize: maxUploadMB * 1024 * 1024 }
 
 function uploadFile(req, res, next) {
-    console.log('HERE is MULTER!');  // for testing purposes
+    // console.log('HERE is MULTER!');  // for testing purposes
 
     const upload = multer({storage, limits: limits}).fields([{name: 'files', maxCount: maxFileNumber}, {name: 'data', maxCount: maxFileNumber}]);
 
     upload(req, res, function (err) {
         if (err instanceof multer.MulterError) {
             // A Multer error occurred when uploading.
-            console.log('MULTER ERROR')  // for testing purposes
+            // console.log('MULTER ERROR')  // for testing purposes
             err.status = 400;  // denotes bad request
             return next(err);
 
         } else if (err) {
             // An unknown error occurred when uploading.
-            console.log('NON-MULTER ERROR')  // for testing purposes
+            // console.log('NON-MULTER ERROR')  // for testing purposes
             return next(err);
         }
         else {
             // Everything went fine. 
-            console.log('MULTER REQ PRINTER BODY: ', req.body);
-            console.log('MULTER REQ PRINTER FILES: ', req.files);
+            // console.log('MULTER REQ PRINTER BODY: ', req.body);
+            // console.log('MULTER REQ PRINTER FILES: ', req.files);
     
             return next();
         }
