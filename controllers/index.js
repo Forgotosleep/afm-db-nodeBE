faf1c4d8a61281d3d56b9390b6f9d7c21cea98f0
Brian <markbrian33@gmail.com>
2023-08-09 10:15:19 +0000

    Checkpoint. (somewhat) stable version, including type:module in next iteration so change from require to import ahead

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
 const connection = require("../services/connectDB");
 const moment = require("moment");
+const fs = require("fs");
+const {FormData} = require("formdata-node")
+// const { Blob } = require("fetch-blob")
+// import { Blob } from "fetch-blob";
+// const { createTemporaryBlob, createTemporaryFile } = require("fetch-blob/from")
+// const { File } = require("fetch-blob/file.js")
 
 class mainController {
     static async test(req, res, next) {
@@ -44,28 +50,28 @@ class mainController {
         }
     };
 
-    static async list(path, res) {
-        console.log("THIS IS MAIN CONTROLLER LIST");
+    // static async list(path, res) {  // Depreciated, switched from using 'path' to 'parent ID'
+    //     console.log("THIS IS MAIN CONTROLLER LIST");
 
-        connection.query(
-            "call xxx_cms_List_old(?, @e)",
-            [path],
-            function (err, result) {
-                if (err) {
-                    // console.log("err:", err);
-                    res.error(err?.message || "List error");
-                } else {
-                    // console.log("results:", result);  // for testing purposes
+    //     connection.query(
+    //         "call xxx_cms_List_old(?, @e)",
+    //         [path],
+    //         function (err, result) {
+    //             if (err) {
+    //                 // console.log("err:", err);
+    //                 res.error(err?.message || "List error");
+    //             } else {
+    //                 // console.log("results:", result);  // for testing purposes
 
-                    const data = {
-                        result: result[0],
-                    };
+    //                 const data = {
+    //                     result: result[0],
+    //                 };
 
-                    res.send(data);
-                }
-            }
-        );
-    }
+    //                 res.send(data);
+    //             }
+    //         }
+    //     );
+    // }
 
     static async listByIds(params, res) {
         // console.log('THIS IS MAIN CONTROLLER LIST BY IDS');
@@ -95,49 +101,50 @@ class mainController {
         );
     }
 
-    static async uploadOld(req, res, next) {
-        const files = req?.files;
+    // static async uploadOld(req, res, next) {  // Depreciated, switched to new SP with multiple files upload capability
+    //     const files = req?.files;
 
-        // console.log("CONTROLLER UPLOAD FILES: ", files);  // for testing purposes
+    //     // console.log("CONTROLLER UPLOAD FILES: ", files);  // for testing purposes
 
-        if (!files[0]) {
-            // res.send("ERROR, NO FILE FOUND");
-            // console.log('HERE');  // for testing purposes
-            const err = {
-                status: 400,
-                code: "FILE_NOT_FOUND",
-                message: "File not found",
-            };
-            return next(err);
-        }
+    //     if (!files[0]) {
+    //         // res.send("ERROR, NO FILE FOUND");
+    //         // console.log('HERE');  // for testing purposes
+    //         const err = {
+    //             status: 400,
+    //             code: "FILE_NOT_FOUND",
+    //             message: "File not found",
+    //         };
+    //         return next(err);
+    //     }
 
-        const filename = files[0].originalname;
-        const type = files[0].mimetype;
-        const buffer = files[0].buffer;
-        const size = files[0].size;
-        const bufferBase64 = buffer.toString("base64");
+    //     const filename = files[0].originalname;
+    //     const type = files[0].mimetype;
+    //     const buffer = files[0].buffer;
+    //     const size = files[0].size;
+    //     const bufferBase64 = buffer.toString("base64");
 
-        console.log("CONTROLLER UPLOAD BLOB: ", bufferBase64); // for testing purposes
+    //     console.log("CONTROLLER UPLOAD BLOB: ", bufferBase64); // for testing purposes
 
-        connection.query(
-            "call xxx_cms_UploadFile(?, ?, @e)",
-            [12, bufferBase64],
-            function (err, result) {
-                if (err) {
-                    console.log("err:", err);
-                    next(err);
-                } else {
-                    res.status(201).json({
-                        message: "Success upload file to DB",
-                    });
-                }
-            }
-        );
-    }
+    //     connection.query(
+    //         "call xxx_cms_UploadFile(?, ?, @e)",
+    //         [12, bufferBase64],
+    //         function (err, result) {
+    //             if (err) {
+    //                 console.log("err:", err);
+    //                 next(err);
+    //             } else {
+    //                 res.status(201).json({
+    //                     message: "Success upload file to DB",
+    //                 });
+    //             }
+    //         }
+    //     );
+    // }
 
     static async upload(req, res, next) {
         console.log("HERE is UPLOAD!");
         const files = req.files.files; // I know it's ugly, but it's the most optimal form due to client-side request tampering. But hey, if it works, it works.
+        console.log("HERE is UPLOAD FILES: ", files);
         const body = req.body;
         const {
             siteId,
@@ -200,7 +207,8 @@ class mainController {
                 type: mimetype,
                 size: size,
                 encoding: encoding,
-                data: bufferBase64,
+                // data: bufferBase64,
+                data: buffer.toString(),
             };
             return processedFile;
         };
@@ -224,10 +232,11 @@ class mainController {
             );
         };
 
-        function finishingUp () {
+        function finishingUp() {
+            // TODO Create an error handler logic for partial upload fails
             return res.status(201).json({
                 success: true,
-                data: {success, fail, result},
+                data: { success, fail, result },
             });
         }
         /* Helper functions end */
@@ -254,27 +263,30 @@ class mainController {
 
         /* Insert DB Loop here */
         argFiles.result.forEach((argFile, index) => {
-            console.log('INDEX:', index);
-            let connect =  connection
-                .query(
-                    "call xxx_cms_create(?, ?)",
-                    [args, JSON.stringify(argFile)],
-                    function (err, result) {
-                        const filename = argFile.name;
-                        if (err) {
-                            console.log("Upload Failed, err:", err);
-                            fail.push(filename);
-                        } else {
-                            result = result;
-                            success.push(filename);
+            console.log("INDEX:", index);
+            console.log("ARGFILE BUFFER: ", argFile.buffer);
+            console.log(
+                "ARGFILE BUFFER STRINGIFIED: ",
+                JSON.stringify(argFile.buffer)
+            );
+            let connect = connection.query(
+                "call xxx_cms_create(?, ?)",
+                [args, JSON.stringify(argFile)],
+                function (err, result) {
+                    const filename = argFile.name;
+                    if (err) {
+                        console.log("Upload Failed, err:", err);
+                        fail.push(filename);
+                    } else {
+                        result = result;
+                        success.push(filename);
 
-                            if(index == argFiles.result.length - 1) {
-                                finishingUp();
-                            }
+                        if (index == argFiles.result.length - 1) {
+                            return finishingUp();
                         }
                     }
-                )
-            
+                }
+            );
         });
 
         return;
@@ -286,7 +298,7 @@ class mainController {
         console.log("This is the MAIN CONTROLLER TEST FETCH IMAGE"); // for testing purposes
 
         const data = connection.query(
-            "SELECT * FROM cms_documents_data WHERE id = 2",
+            "SELECT * FROM cms_documents_data WHERE cms_document_id = 107",
             function (err, result) {
                 if (err) {
                     console.log("err:", err);
@@ -294,11 +306,23 @@ class mainController {
                 } else {
                     console.log("DATA: ", result[0].data);
 
+                    let buffer = new Buffer.from(result[0].data, "base64");
+                    console.log("BUFFER: ", buffer);
+                    let image = buffer.toString("base64");
+                    // console.log('IMAGE: ', image);
+                    //     const bufferBase64 = buffer.toString("base64");
+
                     res.send(
-                        `<img src="data:${"image/jpg"};base64,${
-                            result[0].data
-                        }" />`
+                        // Original
+                        `<img src="data:${"image/jpg"};base64,${image}" />`
                     );
+
+                    // res.send(
+                    //     // Original 106
+                    //     `<img src="data:${"image/jpg"};base64,${
+                    //         result[0].data
+                    //     }" />`
+                    // );
 
                     // res.status(200).json({
                     //     message: "Fetch Image success",
@@ -307,6 +331,133 @@ class mainController {
                 }
             }
         );
+    }
+
+    static async download(req, res, next) {
+        // unfinished prototype
+        console.log("DL Req URL: ", req.url);
+        console.log("DL Req QUERY: ", req.query);
+        console.log("DL Req BODY: ", req.body);
+        // console.log("DL Fetch data: ", fetchData);
+
+        // const query = await this.fetchData(req.query.fileId);
+        // let queryResult = query[0][0];
+
+        // let file = await fs.writeFile(
+        //     queryResult.name,
+        //     base64,
+        //     { encoding: "base64" },
+        //     (err) => {
+        //         if (err) {
+        //             console.log("err:", err);
+        //             next(err);
+        //         }
+        //     }).then(function(filefile) {
+        //         console.log("File created");
+        //             console.log("DATA ", filefile);
+
+        //             res.setHeader(
+        //                 "Content-disposition",
+        //                 "attachment; filename=" + queryResult.name
+        //             );
+        //             res.setHeader("Content-type", queryResult.mime_types);
+        //             console.log(filefile);
+        //             return res.send(filefile);
+        //     })
+
+        //     ;
+
+        /* Helper function start */
+        async function blob2file(blobData) {
+            let blob = await createTemporaryBlob(blobData)
+            const fd = new FormData();
+            fd.set("a", blob, "filename");
+            return fd.get("a");
+        }
+
+        function blobToFile(theBlob, fileName, type){       
+            return new File([theBlob], fileName, { type: type })
+        }
+        /* Helper function end */
+
+        console.log('PILLS HERE');
+
+        const data = await connection.query(
+            "call xxx_cms_FetchFile(?)",
+            [parseInt(req.query.fileId)],
+            (err, result) => {
+                if (err) {
+                    console.log("err:", err);
+                    next(err);
+                }
+                else {
+                    let dataFile = result[0][0];
+                    console.log('dataFile: ', dataFile);
+                    let fileData = dataFile.data;
+                    let blobData = new Buffer.from(dataFile.data)
+                    let becomeFile = blob2file(blobData);
+                    // let becomeFile2 = blobToFile(blobData, dataFile.name, dataFile.mime_types);
+
+                    res.send(becomeFile);
+                }
+            }
+        );
+
+        // const data = await connection.query(
+        //     "call xxx_cms_FetchFile(?)",
+        //     [parseInt(req.query.fileId)],
+        //     async function (err, result) {
+        //         if (err) {
+        //             console.log("err:", err);
+        //             next(err);
+        //         } else {
+        //             let dataFile = result[0][0];
+        //             console.log("Fetch RESULT: ", dataFile);
+
+        //             let base64 = dataFile.data;
+        //             console.log("Fetch RESULT: ", dataFile.data);
+        //             console.log("Fetch RESULT: ", base64);
+        //             let file2 = fs.writeFileSync(dataFile.name, base64, {
+        //                 encoding: "base64",
+        //             });
+        //             // let file = fs.writeFileSync(
+        //             //     dataFile.name,
+        //             //     base64,
+        //             //     { encoding: "base64" },
+        //             // );
+        //             console.log("File created");
+        //             console.log("DATA ", file2);
+
+        //             res.setHeader(
+        //                 "Content-disposition",
+        //                 "attachment; filename=" + dataFile.name
+        //             );
+        //             res.setHeader("Content-type", dataFile.mime_types);
+        //             console.log(file2);
+        //             return res.send(file2);
+        //         }
+        //     }
+        // );
+    }
+
+    async fetchData(fileId) {
+        // For testing purposes
+        connection.query(
+            "call xxx_cms_FetchFile(?)",
+            [fileId],
+            function (err, result) {
+                if (err) console.log(err);
+                else {
+                    return result;
+                }
+            }
+        );
+    }
+
+    static async writeFile(name, data) {
+        // For testing purposes
+        let result = await fs.writeFile(name, data, { encoding: "base64" });
+        return result;
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,11 @@ const controller = require("../controllers");
 route.get("/test", controller.test);
 route.get("/test-image", controller.testFetchImage);
 route.get("/test-procedure", controller.testProcedure);
+route.get("/dl", controller.download);
 
 route.post("/cms", controller.commandCenter);
-
 route.post("/upload", controller.upload);
+
+// route.post("/test-upload", controller.testUploadImage);
 
 module.exports = route;
